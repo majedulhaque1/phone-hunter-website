fa46a3ed04800c4e8968f89b70638ae9f78f39a9
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-03-01 20:16:53 +0600

    some style change

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const loadPhones = async () =>{
     const data = await res.json();
     displayPhones(data.data);
 }
-
+// show display phones
 const displayPhones = (phonesData) =>{
     console.log(phonesData.length);
     if(phonesData.length == 0){
@@ -36,7 +36,6 @@ const displayPhones = (phonesData) =>{
                     <h5 class="card-title custom-text-style">${phone.phone_name}</h5>
                     <p class="card-text">Brand: ${phone.brand}</p>
                     <button onclick="loadPhoneDetails('${phone.slug}')" class="btn custom-color fs-5 px-3 py-1" type="button">Details</button>
-                    <a onclick="loadPhoneDetails('${phone.slug}')" href="#phone-details-card" class="btn custom-color">Details</a>
                 </div>
             </div>
             `;
@@ -44,16 +43,15 @@ const displayPhones = (phonesData) =>{
             
         });
     }
-
 }
-
+// load phone details
 const loadPhoneDetails = async (phoneId) =>{
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhoneDetails(data.data);
 }
-
+// display phone details
 const displayPhoneDetails = (phoneIdData) =>{
     console.log(phoneIdData);
     const phoneDetails = document.getElementById('phone-details');
@@ -64,9 +62,9 @@ const displayPhoneDetails = (phoneIdData) =>{
     <div id="phone-details-card" class="card custom-card-details-style border-0 p-3 shadow-lg mx-auto">
         <img src="${phoneIdData.image}" class="card-img-top p-3" alt="">
         <div class="card-body">
-            <h3 class="card-title">Brand: ${phoneIdData.brand}</h3>
-            <p class="card-text fs-5">Phone Name: ${phoneIdData.name}</p>
-            <p class="card-text fs-5">${phoneIdData.releaseDate ? phoneIdData.releaseDate: 'Release date not found'}</p>
+            <p class="card-text fs-2">${phoneIdData.name}</p>
+            <h3 class="card-title fs-5">Brand: ${phoneIdData.brand}</h3>
+            <p class="card-text fs-5">Release Date: ${phoneIdData.releaseDate ? phoneIdData.releaseDate: 'Release date not found'}</p>
             <h3 class="card-title mb-3">Main Features:</h3>
             <p class="pe-2 fs-normal"><span class="fs-6 pe-2 fw-bold">Storage:</span>${phoneIdData.mainFeatures.storage}</p>
             <p class="pe-2 fs-normal"><span class="fs-6 pe-2 fw-bold">Display-Size:</span>${phoneIdData.mainFeatures.displaySize}</p>
